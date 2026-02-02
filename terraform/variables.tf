b7cd50ad03a728f9ce4bc1c63a3eee338f4ae1a6
Pranab Sethi <pranab2108.codes@gmail.com>
2026-02-02 21:55:50 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,5 @@
 
+
 variable "aws_region" {
   description = "AWS region where resources will be provisioned"
   default     = "eu-west-1"
@@ -18,3 +19,4 @@ variable "my_enviroment" {
   description = "Instance type for the EC2 instance"
   default     = "dev"
 }
+
